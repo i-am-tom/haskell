f8205f2993e8aea079640be7556fc8320c3ef4ce
Tom Harding <i.am.tom.harding@gmail.com>
2023-12-27 11:38:22 +0000

    Tidy up events, standard Phil acknowledgement

--- event/README.md
+++ event/README.md
@@ -1,4 +1,11 @@
 # Representing `event` streams
 
+Years ago, Phil Freeman wrote [the only event library I'll ever
+love](https://github.com/paf31/purescript-event), and I've been copying bits
+out of it ever since. This is all the stuff that I needed in Haskell. You can
+safely bet money that I'll eventually end up porting behaviours here too.
+
+---
+
 A `Source` is a producer of events to which we can subscribe. We can combine
 sources monoidally, and we can filter the events they output.
--- event/tests/Main.hs
+++ event/tests/Main.hs
@@ -1,44 +1,100 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ViewPatterns #-}
 module Main where
 
+import Control.Applicative ((<|>), empty, liftA2)
+import Control.Monad (guard)
+import Data.Event
+import Data.Foldable (for_)
+import Data.Functor (void)
+import Data.IORef (atomicModifyIORef', newIORef)
 import Test.Hspec (hspec, it, shouldBe)
-import Data.Event qualified as Event
-import Control.Monad (join)
-import Data.IORef (atomicModifyIORef', newIORef, readIORef)
+import Test.Hspec.QuickCheck (prop)
+import Witherable (catMaybes, mapMaybe)
 
 main :: IO ()
 main = hspec do
-  it "once" do
-    let event :: Event.Source Int
-        event = Event.once (pure 1)
+  let shouldYield :: forall x. (Eq x, Show x) => Source x -> [x] -> IO (IO ())
+      shouldYield source values = do
+        queue <- newIORef values
 
-    _ <- Event.subscribe event (shouldBe 1)
-    pure ()
+        subscribe source \x -> do
+          next <- atomicModifyIORef' queue \cs -> (tail cs, head cs)
+          x `shouldBe` next
 
-  it "create" do
-    (source, push) <- Event.create
-    current <- newIORef (1 :: Int)
+      infix 1 `shouldYield`
 
-    _ <- Event.subscribe source \x -> join do
-      atomicModifyIORef' current \y ->
-        ( succ y, x `shouldBe` y )
+  prop "functor" \(xs :: [Int]) (y :: Int) -> do
+    (source, push) <- create
 
-    push 1
-    push 2
-    push 3
+    _ <- fmap (+ y) source `shouldYield` fmap (+ y) xs
+    mapM_ push xs
 
-  it "merge" do
-    current <- newIORef (0 :: Int)
+  prop "semigroup" \(xs :: [Int]) (ys :: [Int]) -> do
+    (xsource, xpush) <- create
+    (ysource, ypush) <- create
 
-    let x :: Event.Source Int
-        x = Event.once (pure 2)
+    _ <- xsource <> ysource `shouldYield` [xs <> ys]
+    xpush xs *> ypush ys
 
-        y :: Event.Source Int
-        y = Event.once (pure 3)
+  it "mempty" $ void do
+    mempty @(Source [Int]) `shouldYield` [[]]
 
-    _ <- Event.subscribe (x <> y) \n ->
-      atomicModifyIORef' current \s ->
-        ( s + n, () )
+  prop "catMaybes" \(xs :: [Maybe Int]) -> do
+    (source, push) <- create
 
-    total <- readIORef current
-    total `shouldBe` 5
+    _ <- catMaybes source `shouldYield` (catMaybes xs)
+    mapM_ push xs
+
+  prop "mapMaybe" \(xs :: [Int]) -> do
+    (source, push) <- create
+
+    let f :: Int -> Maybe Int
+        f x = x <$ guard (mod x 2 == 0)
+
+    _ <- mapMaybe f source `shouldYield` (mapMaybe f xs)
+    mapM_ push xs
+
+  prop "<*>" \(x :: Int) (xss :: [Int]) -> do
+    (xsource, xpush) <- create
+    (ysource, ypush) <- create
+
+    _ <- liftA2 (+) xsource ysource `shouldYield` x : succ x : map succ xss
+    xpush 0 *> ypush x *> xpush 1 *> mapM_ ypush xss
+
+  prop "pure" \(x :: Int) -> void do
+    pure x `shouldYield` [x]
+
+  prop "<|>" \(xs :: [Either Int Int]) -> do
+    (xsource, xpush) <- create
+    (ysource, ypush) <- create
+
+    _ <- xsource <|> ysource `shouldYield` map (either id id) xs
+    mapM_ (either xpush ypush) xs
+
+  it "empty" $ void do
+    empty `shouldYield` [] @Int
+
+  prop "create" \(xs :: [Int]) (ys :: [Int]) -> do
+    (source, push) <- create
+
+    cancel <- source `shouldYield`  xs
+    _ <- source `shouldYield` (xs <> ys)
+
+    mapM_ push xs *> cancel
+
+    _ <- source `shouldYield` ys
+    mapM_ push ys
+
+  prop "fold" \(x :: Int) (xss :: [Int]) -> do
+    (source, push) <- create
+
+    _ <- fold (+) 0 source `shouldYield` scanl (+) x xss
+    mapM_ push (x : xss)
+
+  prop "sample" \(chunks :: [([Int], Int)]) -> do
+    (xsource, xpush) <- create
+    (ysource, ypush) <- create
+
+    _ <- sample ysource (fmap const xsource) `shouldYield` map snd chunks
+    for_ chunks \(xs, x) -> mapM_ xpush xs *> xpush x *> ypush ()
