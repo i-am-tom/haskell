cd8f63cd3202e3db92113f5feb789b9e68f427b3
Tom Harding <i.am.tom.harding@gmail.com>
2024-11-06 05:13:39 -0800

    Tests

--- units/tests/Measurement/Coordinate/SpaceSpec.hs
+++ units/tests/Measurement/Coordinate/SpaceSpec.hs
@@ -1,40 +1,27 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE RecordWildCards #-}
 
-module Measurement.CoordinateSpec where
+module Measurement.Coordinate.SpaceSpec where
 
 import Data.Approx (Approx ((~=)))
 import Hedgehog (Gen, Property, diff, forAll, property)
 import Hedgehog.Gen qualified as Gen
 import Hedgehog.Range qualified as Range
 import Measurement.AngleSpec (gen_radians)
-import Measurement.Coordinate
+import Measurement.Coordinate.Space
 import Measurement.Helpers (law_roundtrip)
 
 {-# ANN module "HLint: use camelCase" #-}
 
-hprop_polar_origin_distance :: Property
-hprop_polar_origin_distance = property do
-  x@(Polar r _) <- forAll gen_polar
-  diff (distance2D x origin2D) (~=) r
-
 hprop_cylindrical_origin_distance :: Property
 hprop_cylindrical_origin_distance = property do
   x@(Cylindrical r _ z) <- forAll gen_cylindrical
-  diff (distance3D x origin3D) (~=) (sqrt (r * r + z * z))
+  diff (distance x origin) (~=) (sqrt (r * r + z * z))
 
 hprop_spherical_origin_distance :: Property
 hprop_spherical_origin_distance = property do
   x@(Spherical ρ _ _) <- forAll gen_spherical
-  diff (distance3D x origin3D) (~=) ρ
-
----
-
-hprop_cartesian_2d_polar :: Property
-hprop_cartesian_2d_polar = law_roundtrip @Polar gen_cartesian_2d
-
-hprop_polar_cartesian_2d :: Property
-hprop_polar_cartesian_2d = law_roundtrip @Cartesian2D gen_polar
+  diff (distance x origin) (~=) ρ
 
 hprop_cartesian_3d_spherical :: Property
 hprop_cartesian_3d_spherical = law_roundtrip @Spherical gen_cartesian_3d
@@ -43,40 +30,24 @@ hprop_cartesian_3d_cylindrical :: Property
 hprop_cartesian_3d_cylindrical = law_roundtrip @Cylindrical gen_cartesian_3d
 
 hprop_spherical_cartesian_3d :: Property
-hprop_spherical_cartesian_3d = law_roundtrip @Cartesian3D gen_spherical
+hprop_spherical_cartesian_3d = law_roundtrip @Cartesian gen_spherical
 
 hprop_spherical_cylindrical :: Property
 hprop_spherical_cylindrical = law_roundtrip @Cylindrical gen_spherical
 
 hprop_cylindrical_cartesian_3d :: Property
-hprop_cylindrical_cartesian_3d = law_roundtrip @Cartesian3D gen_cylindrical
+hprop_cylindrical_cartesian_3d = law_roundtrip @Cartesian gen_cylindrical
 
 hprop_cylindrical_spherical :: Property
 hprop_cylindrical_spherical = law_roundtrip @Spherical gen_cylindrical
 
----
-
-gen_cartesian_2d :: Gen Cartesian2D
-gen_cartesian_2d = do
-  x <- Gen.double (Range.linearFrac 0 100)
-  y <- Gen.double (Range.linearFrac 0 100)
-
-  pure Cartesian2D{ x, y }
-
-gen_polar :: Gen Polar
-gen_polar = do
-  r <- Gen.double (Range.linearFrac 0 100)
-  θ <- gen_radians
-
-  pure Polar{ r, θ }
-
-gen_cartesian_3d :: Gen Cartesian3D
+gen_cartesian_3d :: Gen Cartesian
 gen_cartesian_3d = do
   x <- Gen.double (Range.linearFrac 0 100)
   y <- Gen.double (Range.linearFrac 0 100)
   z <- Gen.double (Range.linearFrac 0 100)
 
-  pure Cartesian3D { x, y, z }
+  pure Cartesian { x, y, z }
 
 gen_cylindrical :: Gen Cylindrical
 gen_cylindrical = do
